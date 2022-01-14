341f2aeffd141d78e73f47ea70185c9b55ec356e
AlyoshaAlekseev <95135297+AlyoshaAlekseev@users.noreply.github.com>
2022-01-14 21:35:14 +0500

    Update Primitive.java

--- src/Primitive/Primitive.java
+++ src/Primitive/Primitive.java
@@ -4,7 +4,7 @@ public class Primitive {
 
     public static void main(String[] args) {
         int sum = 0;
-        for (int m = 1; m <= 4; m++) {
+        for (int m = 1; m <= 5; m++) {
             for (int n = 1; n <= 5; n++) {
                 System.out.println("Полка и ячейка [" + m + "][" + n + "]");
                 sum++;
